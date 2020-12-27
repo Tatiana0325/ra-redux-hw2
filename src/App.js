6fbcd03dcfad0927adc64eb2f47444978928c2d4
Tatiana Krasnova <krasnova-tatyana92@mail.ru>
2020-12-27 20:58:32 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+// eslint-disable-next-line
+import regeneratorRuntime from "regenerator-runtime";
+import ServiceAdd from "./components/ServiceAdd.js";
+import ServiceList from "./components/ServiceList.js";
+import ServiceFiltr from "./components/ServiceFiltr.js";
 
-function App() {
+export default function App() {
+  // const urlEnv = process.env.REACT_APP_URL;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <ServiceFiltr />
+      <ServiceAdd />
+      <ServiceList />
+    </React.Fragment>
   );
 }
-
-export default App;
